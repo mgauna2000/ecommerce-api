aad3f7e190299ae62e8df2ea66fb7d0fc511236b
Martin <martingauna2000@gmail.com>
2022-02-04 22:58:44 -0300

    post y get

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ mongoose
     console.log(err);
   });
 
-app.use("/api/user", userRoute)
+app.use("/api/users", userRoute)
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
